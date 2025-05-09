e4dad014fa698a4d50187a89d072d9b5d1c224d1
Varun Yadav <yadav.varun056038@gmail.com>
2025-05-09 22:05:22 +0530

    Controller to update user profile details done.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 import User from "../models/user.js";
 import { generateToken } from "../config/utils.js";
+import User from "../models/user.js";
+import cloudinary from "../config/cloudinary.js";
 
 /*
     Status Codes
@@ -94,4 +96,42 @@ export const login = async (req, res) => {
 // Controller to check if user is authenticated.
 export const checkAuth = (req, res) => {
   res.json({ success: true, user: req.user });
+};
+
+// Controller to update user profile details.
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic, bio, fullName } = req.body;
+
+    const userId = req.user._id;
+    let updateUser;
+
+    if (!profilePic) {
+      updateUser = await User.findByIdAndUpdate(
+        userId,
+        { bio, fullName },
+        { new: true }
+      );
+    } else {
+      const upload = await cloudinary.uploader.upload(profilePic);
+
+      updateUser = await User.findByIdAndUpdate(
+        userId,
+        { profilePic: upload.secure_url, bio, fullName },
+        { new: true }
+      );
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+      userData: updateUser,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
 };
